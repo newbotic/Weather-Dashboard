c7d55cce180eead478afb33db46f884ec0b530e7
newbotic <like_plus@yahoo.es>
2023-12-16 21:09:11 +0000

    remove new div

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-const cityName = $('#search-input').val().trim();
-
-const apiKey = "e7c4d3c5bf68207d8632346a6c99878f";
-const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=";
-
+    
+    const apiKey = "e7c4d3c5bf68207d8632346a6c99878f";
+    const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+    
+    const cityName = $('#search-input').val().trim();
 
 $('#search-form').on('submit',function(event){
     event.preventDefault();
@@ -11,32 +11,52 @@ $('#search-form').on('submit',function(event){
     
 getDataFromApi (cityName);
 });
+
+// ---------------------------
+
 var cardDiv = $('<div>');
-cardDiv.attr('class', 'card-body')//use addClass
+cardDiv.addClass('card')//use addClass
 // cardDiv.text('Hello');
 $('#forecast').append(cardDiv);
-function getDataFromApi(cityName){
-    // fetch("https://api.openweathermap.org/data/2.5/forecast?q=london&appid=e7c4d3c5bf68207d8632346a6c99878f&units=metric")
-    fetch(queryURL + cityName + `&appid=${apiKey}`)
+// function getDataFromApi(cityName){
+//     // fetch("https://api.openweathermap.org/data/2.5/forecast?q=london&appid=e7c4d3c5bf68207d8632346a6c99878f&units=metric")
+//     fetch(queryURL + cityName + `&appid=${apiKey}`)
     
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    })
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data);
+//     })
     
-}
-//add class aside Area
+// }
+//add class asideArea and mainArea
 $('.col-lg-3').addClass('asideArea');
 $('.col-lg-9').addClass('mainArea');
 
-// Create the weather structure dynamically
 
 
+// Create the first new div
+// var firstNewDiv = $('<div>', {
+//     class: 'newDiv',
+//     text: 'This is the first new div'
+//   });
 
+//   // Create the second new div
+//   var secondNewDiv = $('<div>', {
+//     class: 'newDiv',
+//     text: 'This is the second new div'
+//   });
 
+//   // Append the new divs to the existing mainArea
+//   $('#today').append(firstNewDiv);
+//   $('#forecast').append( secondNewDiv);
 });
+
+
+
+
+
 
 
 
