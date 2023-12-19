eabd43192af4b264f3fb195673292ee15c191905
newbotic <like_plus@yahoo.es>
2023-12-19 18:08:11 +0000

    loading history buttons from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,7 @@ $(document).ready(function () {
   $("#search-form").on("submit", function (event) {
     event.preventDefault();
     const cityName = $("#search-input").val().trim();
-
-    // call getDataFromApi function
-
-    getDataFromApi(cityName);
+    getDataFromApi(cityName); // call getDataFromApi function
   });
 
   let city; //var to store current city
@@ -21,6 +18,10 @@ $(document).ready(function () {
 
   function getDataFromApi(cityName) {
     $("#forecast").empty(); //Clear previous data
+
+    if (!cityName) {
+      cityName = "London";//Default
+    }
 
     fetch(
       "https://api.openweathermap.org/data/2.5/forecast?q=" +
@@ -32,25 +33,26 @@ $(document).ready(function () {
       })
       .then(function (data) {
         renderData(data); //Render data
-        saveDataLocal(city, data); //Save to local storage
-        createBtn(city); //Create a history button
+        saveDataLocal(cityName, data); //Save to local storage
+        createBtn(cityName); // Create history button
+        // renderBtns(); //?????????????????????????
       })
       .catch(function (error) {
         console.error("Error data:", error);
       });
   }
-
-  // Function render data on the page
+  // Render data on the page
 
   function renderData(data) {
     const weatherIcon = $(".weather-icon");
     const list = data.list;
+
     for (let i = 0; i < list.length; i++) {
       const iconCode = list[i].weather[0].icon;
       const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
       $(".weather-icon").attr("src", iconUrl);
 
-      // Render today's data
+      //Render today's data
 
       function renderToday() {
         const todaySection = $("#today");
@@ -85,18 +87,15 @@ $(document).ready(function () {
     }
   }
 
-  //  Save data to local storage 
+  //  Save data to local storage
 
   function saveDataLocal(city, data) {
-    // localStorage.setItem(`${city}-data`, JSON.stringify(data));
-    localStorage.setItem(`${city}`, JSON.stringify(data));
-
+    localStorage.setItem(`${city}-data`, JSON.stringify(data));
   }
 
-// Create history buttons
+  // Create history buttons
 
   function createBtn(city) {
-    //add conditional logic to render btn
     var historyBtn = $("<button>");
     historyBtn.attr("id", "btn-" + city.toLowerCase());
     historyBtn.addClass("historyBtn");
@@ -104,69 +103,76 @@ $(document).ready(function () {
     $("#history").prepend(historyBtn);
   }
 
-  function historyListener(){
-    
-  
-  $("#history").on("click", ".historyBtn", function (event) {
-    event.preventDefault();
-    var city = $(this).text();
-    // getDataFromApi(city); ????? changed
-    getDataFromLocal(city); 
+  //History listener
+  function historyListener() {
+    $("#history").on("click", ".historyBtn", function (event) {
+      event.preventDefault();
+      var city = $(this).text();
+      // getDataFromApi(city); //Get data from API
+      getDataFromLocal(city);
+    });
+  }
 
-  });
-  };
-
-historyListener();
+  //Render buttons
 
   function renderBtns() {
-    // First we get all the items in local storage and get the keys. 
-    //Then filter the list by removing any that do not contain "-data"
-    var history = Object.keys(localStorage).filter((itemInStorage) =>
-      itemInStorage.includes("-data")
-    );
-    history.forEach((city) => createBtn(city.replace("-data", "")));
+    var storageKeys = Object.keys(localStorage);
+    var uniqueCities = new Set();
+
+    storageKeys.forEach(function (key) {
+      if (key.includes("-data")) {
+        const city = key.replace("-data", "");
+        uniqueCities.add(city);
+      }
+    });
+
+    // Clear existing buttons
+
+    $("#history").empty();
+
+    uniqueCities.forEach(function (city) {
+      createBtn(city);
+    });
+
+    const lastCity = storageKeys[storageKeys.length - 1].replace("-data", "");
+    getDataFromLocal(lastCity);
   }
 
-  renderBtns();
-
-
-
-// --function getDataFromLocal--  end
-
-function getDataFromLocal(city) {
-  // console.log(Object.keys(localStorage));
-  if (!city) {
-    var history = Object.keys(localStorage).filter((itemInStorage) =>
-      itemInStorage.includes("-data")
-    );
-    city = history[history.length - 1];
-  }
-  try {
-    const savedData = localStorage.getItem(city);
-    if (savedData) {
-      const parseSavedData = JSON.parse(savedData);
-      // console.log(parseSavedData);
-      renderData(parseSavedData);
-    } else {
-      console.log("No data ", city);
-      return null;
+  function getDataFromLocal(city) {
+    if (!city) {
+      var history = Object.keys(localStorage).filter((itemInStorage) =>
+        itemInStorage.includes("-data")
+      );
+      city =
+        history.length > 0
+          ? history[history.length - 1].replace("-data", "")
+          : "London";
     }
-  } catch (error) {
-    // console.error('Error :', error);
-    return null;
+    try {
+      const savedData = localStorage.getItem(`${city}-data`);
+      if (savedData) {
+        const parseSavedData = JSON.parse(savedData);
+        renderData(parseSavedData);
+      } else {
+        console.log("No data for", city);
+        getDataFromApi(city);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
-}
-getDataFromLocal();
 
+  
+  function titleDiv() {
+    var newDiv = $("<div>").attr("id", "titleForecast");
+    $("#today").after(newDiv);
+    newDiv.text("5-Days Forecast");
+  }
 
-function titleDiv() {
-  var newDiv = $("<div>").attr("id", "titleForecast");
+//set up initial page
 
-  $("#today").after(newDiv);
-  newDiv.text("5-Days Forecast");
-}
-titleDiv();
-
-
+  titleDiv();
+  historyListener();
+  renderBtns();
+  getDataFromApi("London"); 
 });
-
